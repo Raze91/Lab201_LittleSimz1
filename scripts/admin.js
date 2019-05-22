8d43be90d908bbd7e5b9565ea3bc32157499f501
Sami Hattab <hattab_sami@yahoo.fr>
2019-05-22 10:27:54 +0200

    admin page is over, admin.html v1

--- scripts/admin.js
+++ scripts/admin.js
@@ -15,6 +15,8 @@ $('#create').on('submit', CreateDate);
 
 const db = firebase.firestore();
 
+let increment = 1;
+
 function CreateDate() {
   event.preventDefault();
 
@@ -30,8 +32,10 @@ function CreateDate() {
     date: $('#date').val(),
     city_name: $('#city_name').val(),
     country: $('#country').val(),
+    party_place: $('#party_place').val(),
     party_name: $('#party_name').val(),
-    complete: radio
+    complete: radio,
+
   })
     .then(function (docRef) {
       console.log("Document written with ID :", docRef.id);
@@ -44,36 +48,72 @@ function CreateDate() {
 }
 
 function displayTour() {
+  // increment++;
   db.collection("tour").orderBy("date", "asc").get().then((querySnapshot) => {
     let html_tours = '';
     querySnapshot.forEach(function (doc) {
-      const {date, city_name, country, party_name, complete} = doc.data();
+      const { date, city_name, country, party_place,party_name, complete } = doc.data();
 
       html_tours += `
-      <div class="content"><strong>Date du concert :</strong> ${date} <br>
-      <strong>Ville :</strong> ${city_name} <br>
-      <strong>Pays :</strong> ${country} <br>
-      <strong>Lieu :</strong> ${party_name} <br>
-      <strong>Complet ? :</strong> ${complete} <br>
+      <div>
+
+      <strong>Date du concert</strong> <input type="text" class="form-control" value="${date}" id="${'date'+ doc.id}" required autofocus> 
+      
+      <strong>Ville :</strong> <input type="text" class="form-control" value="${city_name}" id="${'city_name'+ doc.id}" required> 
+
+      <strong>Pays :</strong> <input type="text" class="form-control" value="${country}" id="${'country'+ doc.id}" required> 
+
+      <strong>Lieu :</strong> <input type="text" class="form-control" value="${party_place}" id="${'party_place'+ doc.id}" required>
+
+      <strong>Nom du festival</strong> <input type="text" class="form-control" value="${party_name}" id="${'party_name'+ doc.id}" required>
+
+      <strong>Complet ? :</strong> <input type="text" class="form-control" value="${complete}" id="${'soldOut'+ doc.id}" required> <br> 
+
+      <button onclick="updateData(this.id)" class="btn btn-sm btn-primary btn-block" id="${doc.id}">Modifier</button>
+
       <button onclick="deleteData(this.id)" class="btn btn-sm btn-primary btn-block" id="${doc.id}">Supprimer</button>
       </div>
       <br>`;
+      // increment++;
     });
 
     $('#displayTours').html(html_tours);
-    
   });
 }
 
 function deleteData(id) {
-  db.collection("tour").doc(id).delete()
+  event.preventDefault();
   
-  .then(function() {
-    console.log("Document successfully deleted!");
-}).catch(function(error) {
-    console.error("Error removing document: ", error);
-});
-displayTour();
+  db.collection("tour").doc(id).delete()
+
+    .then(function () {
+      console.log("Document successfully deleted!");
+    }).catch(function (error) {
+      console.error("Error removing document: ", error);
+    });
+  displayTour();
+}
+
+function updateData(id) {
+  event.preventDefault();
+
+  db.collection("tour").doc(id).update({
+    date: $('#date'+ id).val(),
+    city_name: $('#city_name'+ id).val(),
+    country: $('#country'+ id).val(),
+    party_place: $('#party_place' + id).val(),
+    party_name: $('#party_name'+ id).val(),
+    complete: $('#soldOut'+ id).val(),
+  })
+    .then(function () {
+      console.log("Document successfully updated!");
+    })
+    .catch(function (error) {
+      // The document probably doesn't exist.
+      console.error("Error updating document: ", error);
+    });
+
+  displayTour();
 }
 
 function onSubmitLogin(event) {
